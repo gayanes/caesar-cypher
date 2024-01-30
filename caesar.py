d596712deb3a6bf1d0d346879e0f98dd1e58096b
gayanes <gayanesance@gmail.com>
2024-01-29 20:37:49 -0500

    implemented encrypt and decrypt functions

--- caesar.py
+++ caesar.py
@@ -2,10 +2,31 @@ import sys
 
 
 def encrypt(message, k):
-    return message
+        # (% 26) + 71
+    encode = [] # empty list for encoded characters
+
+    for i in message:
+        if i.isalpha() and i.islower(): # checks if char is a letter, lowercase
+            num = ord(i) # gets ascii number for the letter
+            enc_letter = ((num + k - 97) % 26) + 97 # calculates encrypted ascii code
+            encode.append(chr(enc_letter)) # turns ascii number into letter
+        else:
+            encode.append(i) # appends all other characters
+    
+    return "".join(encode) 
 
 def decrypt(message, k):
-    return message
+    decode = []
+
+    for i in message:
+        if i.isalpha() and i.islower(): # checks if character is a letter, lowercase
+            num = ord(i)
+            dec_letter = ((num - k - 97) % 26) + 97 # calculates decrypted ascii code
+            decode.append(chr(dec_letter)) 
+        else:
+            decode.append(i) # appends all other characters
+
+    return "".join(decode) 
 
 if __name__ == "__main__":
     # take in first arg as word
